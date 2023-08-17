1f4819707b087420b18c3306abcd194dafa9d6a6
gabo9914 <108150837+gabo9914@users.noreply.github.com>
2023-08-16 22:19:22 -0500

    main

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -3,7 +3,7 @@ const LocationInfo = ({ currentLocation }) => {
   return (
     <section className=" info-location w-screen  flex flex-col items-center gap-2">
       <h2 className=' font-bold text-xl '>{currentLocation?.name}</h2>
-      <ul className=" flex  gap-10">
+      <ul className=" flex  gap-10 mb-2">
         <li className=' border-2'>Type: {currentLocation?.type}</li>
         <li className=' border-2'>Dimension: {currentLocation?.dimension}</li>
         <li className=' border-2'>Population: {currentLocation?.residents.length}</li>
